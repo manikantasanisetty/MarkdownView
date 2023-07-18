0f8e148fd8f2ca954b76de76637917d589e16c31
LiYanan2004 <37542129+LiYanan2004@users.noreply.github.com>
2023-07-18 11:42:51 +0800

    visionOS support
    - Updated swift toolchain to 5.9 (Only 5.9 can support visionOS)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,6 +10,7 @@ let package = Package(
       .iOS(.v15),
       .tvOS(.v15),
       .watchOS(.v8),
+      .visionOS(.v1),
     ],
     products: [
         .library(name: "MarkdownView", targets: ["MarkdownView"]),
